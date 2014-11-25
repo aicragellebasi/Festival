e6d838b555402ee33abb98986d80e4997f0af5ee
Isabelle <aicragellebasi@users.noreply.github.com>
2014-11-25 18:53:41 -0500

    Sample of beforeSave
    Commented out. Not currently using this method. It is a sample that Musti did in order to show me how to deal with a virtual field before saving an object

--- cloud/main.js
+++ cloud/main.js
@@ -1,1 +1,12 @@
 require('cloud/app.js');
+
+/*
+var Activity = Parse.Object.extend('Activity');
+Parse.Cloud.beforeSave(Activity, function(req, res){
+	var activity = req.object;
+	//
+	activity.set('isFull',isFull);
+
+	res.success();
+});
+*/
